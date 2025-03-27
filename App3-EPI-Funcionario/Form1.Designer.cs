d4f241eca708a906b479fef75fa33819c3ef4fe4
Otavio Emanoel <otavioemanuel6b@gmail.com>
2025-03-27 13:52:41 -0300

    Conclusão do projeto

--- App3-EPI-Funcionario/Form1.Designer.cs
+++ App3-EPI-Funcionario/Form1.Designer.cs
@@ -29,22 +29,28 @@
         private void InitializeComponent()
         {
             this.groupBox = new System.Windows.Forms.Panel();
+            this.calcularBnt = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.salarioFinalTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.salarioTxt = new System.Windows.Forms.TextBox();
             this.faltasTxt = new System.Windows.Forms.TextBox();
             this.nomeTxt = new System.Windows.Forms.TextBox();
             this.checkEPI = new System.Windows.Forms.CheckBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.salarioFinalTxt = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.calcularBnt = new System.Windows.Forms.Button();
+            this.Otavio = new System.Windows.Forms.Label();
+            this.Davi = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.groupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox
             // 
             this.groupBox.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.groupBox.Controls.Add(this.label5);
+            this.groupBox.Controls.Add(this.Davi);
+            this.groupBox.Controls.Add(this.Otavio);
             this.groupBox.Controls.Add(this.calcularBnt);
             this.groupBox.Controls.Add(this.label4);
             this.groupBox.Controls.Add(this.salarioFinalTxt);
@@ -59,6 +65,69 @@
             this.groupBox.Name = "groupBox";
             this.groupBox.Size = new System.Drawing.Size(776, 426);
             this.groupBox.TabIndex = 0;
+            // 
+            // calcularBnt
+            // 
+            this.calcularBnt.Location = new System.Drawing.Point(46, 315);
+            this.calcularBnt.Name = "calcularBnt";
+            this.calcularBnt.Size = new System.Drawing.Size(225, 36);
+            this.calcularBnt.TabIndex = 9;
+            this.calcularBnt.Text = "Calcular Salário";
+            this.calcularBnt.UseVisualStyleBackColor = true;
+            this.calcularBnt.Click += new System.EventHandler(this.CalcularBnt_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(621, 351);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Salário Final";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // salarioFinalTxt
+            // 
+            this.salarioFinalTxt.Location = new System.Drawing.Point(533, 378);
+            this.salarioFinalTxt.Name = "salarioFinalTxt";
+            this.salarioFinalTxt.ReadOnly = true;
+            this.salarioFinalTxt.Size = new System.Drawing.Size(225, 20);
+            this.salarioFinalTxt.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(42, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Qual o seu salário?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(42, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Quantas faltas?";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(42, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nome do Funcionário";
             // 
             // salarioTxt
             // 
@@ -93,68 +162,38 @@
             this.checkEPI.Text = "Usou EPI?";
             this.checkEPI.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // Otavio
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(42, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(229, 24);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Nome do Funcionário";
+            this.Otavio.AutoSize = true;
+            this.Otavio.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Otavio.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Otavio.Location = new System.Drawing.Point(567, 62);
+            this.Otavio.Name = "Otavio";
+            this.Otavio.Size = new System.Drawing.Size(109, 37);
+            this.Otavio.TabIndex = 10;
+            this.Otavio.Text = "Otavio";
             // 
-            // label2
+            // Davi
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(42, 113);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(167, 24);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Quantas faltas?";
+            this.Davi.AutoSize = true;
+            this.Davi.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Davi.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Davi.Location = new System.Drawing.Point(596, 104);
+            this.Davi.Name = "Davi";
+            this.Davi.Size = new System.Drawing.Size(80, 37);
+            this.Davi.TabIndex = 11;
+            this.Davi.Text = "Davi";
             // 
-            // label3
+            // label5
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label3.Location = new System.Drawing.Point(42, 179);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(203, 24);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Qual o seu salário?";
-            // 
-            // salarioFinalTxt
-            // 
-            this.salarioFinalTxt.Location = new System.Drawing.Point(533, 378);
-            this.salarioFinalTxt.Name = "salarioFinalTxt";
-            this.salarioFinalTxt.Size = new System.Drawing.Size(225, 20);
-            this.salarioFinalTxt.TabIndex = 7;
-            this.salarioFinalTxt.Visible = false;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label4.Location = new System.Drawing.Point(621, 351);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(137, 24);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Salário Final";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
-            // 
-            // calcularBnt
-            // 
-            this.calcularBnt.Location = new System.Drawing.Point(46, 315);
-            this.calcularBnt.Name = "calcularBnt";
-            this.calcularBnt.Size = new System.Drawing.Size(127, 36);
-            this.calcularBnt.TabIndex = 9;
-            this.calcularBnt.Text = "Calcular Salário";
-            this.calcularBnt.UseVisualStyleBackColor = true;
-            this.calcularBnt.Click += new System.EventHandler(this.CalcularBnt_Click);
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(551, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 37);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Samuel";
             // 
             // appForm3
             // 
@@ -163,6 +202,7 @@
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.groupBox);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
             this.Name = "appForm3";
             this.Text = "Verificar EPI";
             this.groupBox.ResumeLayout(false);
@@ -184,6 +224,9 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Davi;
+        private System.Windows.Forms.Label Otavio;
     }
 }
 
--- App3-EPI-Funcionario/Form1.cs
+++ App3-EPI-Funcionario/Form1.cs
@@ -19,19 +19,35 @@ namespace App3_EPI_Funcionario
 
         private void CalcularBnt_Click(object sender, EventArgs e)
         {
-            string nome = nomeTxt.Text;
-            int faltas = Convert.ToInt32(faltasTxt.Text);
-            int salario = Convert.ToInt32(salarioTxt.Text);
-            salarioFinalTxt.Text = "";
-
-            if (checkEPI.Checked == true)
+            try
             {
-                int salarioFinal = salario + 500;
-                salarioFinalTxt.Text = $"{salarioFinal}";
-                salarioFinalTxt.Text = $"salarioFinal";
-                MessageBox.Show($"Nome: {nome} \n Faltas: {faltas} \n Salário: {salario} \n Salario final: {salarioFinal}");
-                return;
+                string nome = nomeTxt.Text;
+                int faltas = Convert.ToInt32(faltasTxt.Text);
+                int salario = Convert.ToInt32(salarioTxt.Text);
+                salarioFinalTxt.Text = "";
 
+                if (checkEPI.Checked == true)
+                {
+                    int salarioFinal = salario + 500;
+                    salarioFinalTxt.Text = $"";
+                    salarioFinalTxt.Text = $"{salarioFinal}";
+                    MessageBox.Show($"Nome: {nome} \n Faltas: {faltas} \n Salário: {salario} \n Salario final: {salarioFinal} \n Continue usando o EPI!");
+                    return;
+
+                }
+                else
+                {
+                    int salarioFinal = salario;
+                    salarioFinalTxt.Text = $"";
+                    salarioFinalTxt.Text = $"{salarioFinal}";
+                    MessageBox.Show($"Nome: {nome} \n Faltas: {faltas} \n Salário: {salario} \n Salario final: {salarioFinal} \n Favor utilizar o EPI...");
+                    return;
+                }
+            }
+            catch (Exception error)
+            {
+                salarioFinalTxt.Text = $"";
+                MessageBox.Show("Houve um erro " + error);
             }
         }
     }
